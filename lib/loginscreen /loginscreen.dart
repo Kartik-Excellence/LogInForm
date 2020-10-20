b5893aaa88f7253e32dff4edf4d07879edcb6140
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-10-20 13:01:58 +0530

    VAlidation complete

--- lib/loginscreen /loginscreen.dart
+++ lib/loginscreen /loginscreen.dart
@@ -41,6 +41,8 @@ class Login extends StatelessWidget {
   }
 }
 
+final _loginFormKey = GlobalKey<FormState>();
+
 class LoginScreen extends StatelessWidget {
   String email;
   String password;
@@ -49,18 +51,25 @@ class LoginScreen extends StatelessWidget {
   TextEditingController textEditingControll = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    final emailField = TextField(
+    final emailField = TextFormField(
       controller: textEditingController,
       obscureText: false,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           prefixIcon: new Icon(Icons.mail),
           hintText: "Email",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0),
+              borderSide: new BorderSide(color: Colors.red))),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Enter Email';
+        }
+        return null;
+      },
     );
 
-    final passwordField = TextField(
+    final passwordField = TextFormField(
       controller: textEditingControll,
       obscureText: true,
       decoration: InputDecoration(
@@ -70,29 +79,42 @@ class LoginScreen extends StatelessWidget {
           prefixIcon: new Icon(Icons.lock),
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Enter Password';
+        }
+        return null;
+      },
     );
 
     return Scaffold(
       body: Column(
         children: <Widget>[
           Login(),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: emailField,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            alignment: Alignment.centerRight,
-            child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: passwordField,
+          Form(
+            key: _loginFormKey,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Card(
+                    shadowColor: Colors.black,
+                    elevation: 8,
+                    shape: StadiumBorder(),
+                    child: emailField,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  alignment: Alignment.centerRight,
+                  child: Card(
+                    shadowColor: Colors.black,
+                    elevation: 8,
+                    shape: StadiumBorder(),
+                    child: passwordField,
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
@@ -100,6 +122,7 @@ class LoginScreen extends StatelessWidget {
               alignment: Alignment.centerRight,
               child: Text('ForgotPassword')),
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 80),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(45.0),
                 gradient: LinearGradient(
@@ -115,16 +138,18 @@ class LoginScreen extends StatelessWidget {
               child: Text('Login'),
               textColor: Colors.white,
               onPressed: () {
-                String mail = "@gmail.com";
                 email = textEditingController.text;
-                if (email.contains(mail)) {
-
-                  print('valid email');
+                password = textEditingControll.text;
+                if (_loginFormKey.currentState.validate()) {
+                  print(email + '\n' + password);
                 } else
-                  print('invalid mail');
+                  return null;
               },
             ),
           ),
+          SizedBox(
+              height: 24,
+            ),
           RichText(
               text: TextSpan(children: <TextSpan>[
             TextSpan(text: 'Dont have account:'),
--- lib/signupscreen/signupscreen.dart
+++ lib/signupscreen/signupscreen.dart
@@ -24,18 +24,21 @@ class Signup extends StatelessWidget {
               child: Image.asset('assets/IMG_20201015_184423.png'),
             ),
             Container(
-              padding: EdgeInsets.only(right: 30),
-              child: Text(
-                'Register',
-                style:
-                    TextStyle(color: Colors.white,fontSize:20, fontWeight: FontWeight.bold),
-              ),
-              alignment: Alignment.bottomRight
-            )
+                padding: EdgeInsets.only(right: 30),
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                alignment: Alignment.bottomRight)
           ],
         ));
   }
 }
+
+final _formkey = GlobalKey<FormState>();
 
 class SignupScreen extends StatelessWidget {
   TextEditingController getUserName = new TextEditingController();
@@ -43,10 +46,10 @@ class SignupScreen extends StatelessWidget {
   TextEditingController getContact = new TextEditingController();
   TextEditingController getEmail = new TextEditingController();
   String setUsername, setPassword, setContact, setEmail;
-
+  ScrollController scrollController;
   @override
   Widget build(BuildContext context) {
-    final fullName = TextField(
+    final fullName = TextFormField(
       controller: getUserName,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -54,134 +57,192 @@ class SignupScreen extends StatelessWidget {
           hintText: "FullName",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (value) {
+        setUsername = getUserName.text;
+        if (value.isEmpty) {
+          return 'Enter Username';
+        }
+        return null;
+      },
     );
 
-    final emailField = TextField(
+    final emailField = TextFormField(
       controller: getEmail,
       obscureText: false,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           prefixIcon: Icon(Icons.mail),
-         // icon: new Icon(Icons.mail),
+          // icon: new Icon(Icons.mail),
           hintText: "Email",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (value) {
+        setEmail = getEmail.text;
+        if (value.isEmpty) {
+          return 'Enter Email';
+        } else {
+          String mail = "@gmail.com";
+
+          if (setEmail.contains(mail)) {
+          } else {
+            return 'Enter Valid mail';
+          }
+        }
+        return null;
+      },
     );
 
-    final phoneNUmber = TextField(
-      controller: getContact,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "phoneNumber",
-          
-          prefixIcon: Icon(Icons.contact_phone),
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
+    final phoneNUmber = TextFormField(
+        controller: getContact,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "phoneNumber",
+            prefixIcon: Icon(Icons.contact_phone),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        validator: (value) {
+          setContact = getContact.text;
+          if (value.isEmpty) {
+            return 'Enter PhoneNUmber';
+          } else {
+            Pattern phonePattern = r'^(?=.*?[0-9]).{10,}$';
+            RegExp phoneExp = new RegExp(phonePattern);
+            if (phoneExp.hasMatch(setContact)) {
+            } else {
+              return 'Enter Valid Contact';
+            }
+          }
+          return null;
+        });
 
-    final passwordField = TextField(
+    final passwordField = TextFormField(
       controller: getPassword,
-      obscureText: false,
+      obscureText: true,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: "Password",
           prefixIcon: new Icon(Icons.lock),
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (value) {
+        setPassword = getPassword.text;
+        if (value.isEmpty) {
+          return 'Enter Password';
+        } else {
+          Pattern passwordPattern =
+              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+          RegExp regExp = new RegExp(passwordPattern);
+          if (regExp.hasMatch(setPassword)) {
+          } else {
+            return 'Enter valid Password';
+          }
+        }
+        return null;
+      },
     );
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Signup(),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              alignment: Alignment.centerRight,
-               child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: fullName ,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Signup(),
+            Form(
+                key: _formkey,
+                child: Column(
+                  //mainAxisAlignment:  MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                    
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                      alignment: Alignment.centerRight,
+                      child: Card(
+                        shadowColor: Colors.black,
+                        elevation: 8,
+                        shape: StadiumBorder(),
+                        child: fullName,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                      alignment: Alignment.centerRight,
+                      child: Card(
+                        shadowColor: Colors.black,
+                        elevation: 8,
+                        shape: StadiumBorder(),
+                        child: emailField,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                      alignment: Alignment.centerRight,
+                      child: Card(
+                        shadowColor: Colors.black,
+                        elevation: 8,
+                        shape: StadiumBorder(),
+                        child: phoneNUmber,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                      alignment: Alignment.centerRight,
+                      child: Card(
+                        shadowColor: Colors.black,
+                        elevation: 8,
+                        shape: StadiumBorder(),
+                        child: passwordField,
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45.0),
+                  gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.orange[900],
+                        Colors.orange[800],
+                        Colors.orange[700],
+                        Colors.orange[600]
+                      ])),
+              child: FlatButton(
+                  child: Text('Signup'),
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  onPressed: () {
+                    if (_formkey.currentState.validate()) {
+                      //print('tapped');
+                      print(setEmail +
+                          '\n' +
+                          setUsername +
+                          '\n' +
+                          setContact +
+                          '\n' +
+                          setPassword);
+                    } else
+                      return null;
+                  }),
             ),
-              ),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              alignment: Alignment.centerRight,
-               child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: emailField ,
+            SizedBox(
+              height: 24,
             ),
-              ),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              alignment: Alignment.centerRight,
-              child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: phoneNUmber ,
-            ),
-              ),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              alignment: Alignment.centerRight,
-               child: Card(
-              shadowColor: Colors.black,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: passwordField,
-            ),
-              ),
-          Container(
-             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(45.0),
-              gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  Colors.orange[900],
-                  Colors.orange[800],
-                  Colors.orange[700],
-                  Colors.orange[600]
-                ])
-            ),
-            child: FlatButton(
-              child: Text('Signup'),
-             // color: Colors.orange,
-              textColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              onPressed: () {
-                String mail = "@gmail";
-                setUsername = getUserName.text;
-                setEmail = getEmail.text;
-                setContact = getContact.text;
-                setPassword = getPassword.text;
-                if (setEmail.contains(mail)) {
-                  print('Username:'+setUsername +
-                  '\nE-Mail'+setEmail +
-                  '\nContact'+setContact +
-                  '\nPassword'+setPassword);
-                } else
-                  print('Enter Valid Main Id');
-              },
-            ),
-          ),
-          RichText(text: 
-          TextSpan(children: <TextSpan>[
-            TextSpan(text: 'Already a member:'),
-            TextSpan(text: 'Signin',style: TextStyle(color: Colors.blueGrey),
-            recognizer: TapGestureRecognizer()..onTap=(){
-              Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginScreen() ));
-            }
-            ),
-          ])
-          )
-        ],
+            RichText(
+                text: TextSpan(children: <TextSpan>[
+              TextSpan(text: 'Already a member:'),
+              TextSpan(
+                  text: 'Signin',
+                  style: TextStyle(color: Colors.blueGrey),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginScreen()));
+                    }),
+            ]))
+          ],
+        ),
       ),
     );
   }
